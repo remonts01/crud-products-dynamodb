8cc01a9f8c40be33456ad8a889cca490d8a58f9e
Renan Monteiro <renan.paula@pmrun.com.br>
2024-07-26 18:29:55 -0300

    feat: update application.properties

--- src/main/java/com/renanmonteiro/crud_products_dynamodb/repository/ProductRepository.java
+++ src/main/java/com/renanmonteiro/crud_products_dynamodb/repository/ProductRepository.java
@@ -1,11 +1,13 @@
 package com.renanmonteiro.crud_products_dynamodb.repository;
 
+import org.socialsignin.spring.data.dynamodb.repository.EnableScan;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.renanmonteiro.crud_products_dynamodb.entity.Product;
 
 @Repository
+@EnableScan
 public interface ProductRepository extends CrudRepository<Product, String> {
     
 }
