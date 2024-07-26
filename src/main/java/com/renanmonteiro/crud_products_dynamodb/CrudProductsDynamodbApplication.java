2aa9c16335afb6fa449ebbde544b5e880c8c5475
Renan Monteiro <renan.paula@pmrun.com.br>
2024-07-26 17:08:37 -0300

    initial commit

---
+++ src/main/java/com/renanmonteiro/crud_products_dynamodb/CrudProductsDynamodbApplication.java
@@ -0,0 +1,13 @@
+package com.renanmonteiro.crud_products_dynamodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudProductsDynamodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudProductsDynamodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/renanmonteiro/crud_products_dynamodb/CrudProductsDynamodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.renanmonteiro.crud_products_dynamodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudProductsDynamodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
